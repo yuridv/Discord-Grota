df4c51a4efddab7c49baf56dbd47e130d0fc5de4
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-07-30 16:41:44 -0300

    button register ended

--- src/buttons/register.js
+++ src/buttons/register.js
@@ -1,5 +1,26 @@
+const { MessageFlags, ChannelType, PermissionFlagsBits, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+
 const { Errors } = require('../utils/functions');
-const { MessageFlags, ChannelType, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const config = require('../../config.json');
+
+const asks = [
+  { type: 'id', msg: '• *Qual o seu* ***ID*** *dentro da cidade?*' },
+  { type: 'name', msg: '• *Qual o seu* ***Nome e Sobrenome*** *dentro da cidade?*' },
+  { type: 'phone', msg: '• *Qual o seu* ***Número de Telefone*** *dentro da cidade?*' },
+  { type: 'recruiter', msg: '• *Qual o* ***ID*** *de quem te* ***Recrutou*** *dentro da cidade?*' }
+];
+
+const ordinais = {
+  1: 'Primeira',
+  2: 'Segunda',
+  3: 'Terceira',
+  4: 'Quarta',
+  5: 'Quinta',
+  6: 'Sexta',
+  7: 'Sétima',
+  8: 'Oitava',
+  9: 'Nona'
+};
 
 const command = async(client, interaction) => {
   try {
@@ -9,7 +30,7 @@ const command = async(client, interaction) => {
     const channel = await interaction.guild.channels.create({
       name: `✏️・registro-${interaction.user.id}`,
       type: ChannelType.GuildText,
-      parent: '1399828549371691162',
+      parent: config.register_category,
       permissionOverwrites: [
         { id: interaction.guild.id, deny: [ PermissionFlagsBits.ViewChannel ] },
         { id: interaction.user.id, allow: [ PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages ] }
@@ -23,63 +44,78 @@ const command = async(client, interaction) => {
       flags: MessageFlags.Ephemeral
     });
 
-    let embed = new EmbedBuilder()
-      .setAuthor({ name: 'Registro GROTA・Meta City', iconURL: 'https://media.discordapp.net/attachments/1362573683767640215/1399820068686270484/logo_grota.png?ex=688a6387&is=68891207&hm=e4117c2559984dd42106bfff2d731c5cd3e5eb2d0f74c26c21d6db00074e3083&=&format=webp' })
-      .setColor('#F3A500')
+    const result = {};
+    for (let i = 0; i < asks.length; i++) {
+      let description = '';
+      if (i === 0) description += '> *__Seja bem vindo ao nosso sistema de registro!__*\n\n`Você tem 5 minutos para responder as perguntas!`\n`Somente você e o bot tem acesso a este canal!`\n\n';
+      description += `> **${ordinais[i + 1]} Pergunta:**\n${asks[i].msg}`;
+
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: 'Registro - ' + config.name, iconURL: config.avatar })
+        .setColor(config.color)
+        .setDescription(description);
+
+      const ask = await channel.send({ content: `<@${interaction.user.id}>`, embeds: [ embed ] });
+
+      try {
+        const collected = await channel.awaitMessages({
+          filter: m => m.author.id === interaction.user.id,
+          max: 1,
+          time: 5 * 60 * 1000,
+          errors: [ 'time' ]
+        });
+
+        const msg = collected.first();
+        result[asks[i].type] = msg.content;
+
+        await msg.delete().catch(() => {});
+        await ask.delete().catch(() => {});
+      } catch(err) {
+        return channel.delete().catch(() => {});
+      }
+    }
+
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: 'Registro - ' + config.name, iconURL: config.avatar })
+      .setColor(config.color)
       .setDescription(
-        '> *__Seja bem vindo ao nosso sistema de registro!__*' +
-
-        '\n\n`Você tem 5 minutos para responder as perguntas, somente você e o bot tem acesso a este canal!`' +
-
-        '\n\n> **Primeira Pergunta:**' +
-        '\n• *Qual o seu ID dentro da cidade?*' +
-
+        '> __*Você finalizou o seu registro com sucesso!*__' +
+        '\n\n`Aguarde até que um dos nossos gerente aprove o seu registro...`' +
         '\n\n*__Atenciosamente Grota・Meta City__*'
       );
-    
-    let ask = await channel.send({ content: `<@${interaction.user.id}>`, embeds: [ embed ] });
+    await channel.send({ content: `<@${interaction.user.id}>`, embeds: [ embed ] });
 
-    const filter = m => m.author.id === interaction.user.id;
-    let collector = channel.createMessageCollector({ filter, max: 1, time: 15000 });
+    setTimeout(() => channel.delete().catch(() => {}), 10 * 1000);
 
-    collector.on('collect', async msg => {
-      const ID = msg.content;
+    let answers = '';
+    for (let i = 0; i < asks.length; i++) {
+      answers += `\n> ${asks[i].msg}\n**${result[asks[i].type]}**\n`;
+    }
 
-      await msg.delete();
-      await ask.delete().catch(() => {});
+    const approve = new ButtonBuilder()
+      .setCustomId('approve')
+      .setEmoji('✔️')
+      .setLabel('Aprovar')
+      .setStyle(ButtonStyle.Success);
 
-      embed = new EmbedBuilder()
-        .setAuthor({ name: 'Registro GROTA・Meta City', iconURL: 'https://media.discordapp.net/attachments/1362573683767640215/1399820068686270484/logo_grota.png?ex=688a6387&is=68891207&hm=e4117c2559984dd42106bfff2d731c5cd3e5eb2d0f74c26c21d6db00074e3083&=&format=webp' })
-        .setColor('#F3A500')
-        .setDescription(
-          '> **Segunda Pergunta:**' +
-          '\n• *Qual o seu* ***Nome*** *e* ***Sobrenome*** *dentro da cidade?*'
-        );
+    const reject = new ButtonBuilder()
+      .setCustomId('reject')
+      .setEmoji('✖️')
+      .setLabel('Rejeitar')
+      .setStyle(ButtonStyle.Danger);
 
-      ask = await channel.send({ content: `<@${interaction.user.id}>`, embeds: [ embed ] });
+    const row = new ActionRowBuilder()
+      .addComponents(approve, reject);
 
-      collector = channel.createMessageCollector({ filter, max: 1, time: 15000 });
+    const approval = new EmbedBuilder()
+      .setAuthor({ name: 'Registro - ' + config.name, iconURL: config.avatar })
+      .setColor(config.color)
+      .setDescription(
+        '__*Um novo usuário efetuou o registro!*__\n\n' +
+        answers
+      );
 
-      collector.on('collect', async msg => {
-        const USER = msg.content;
-
-        await msg.delete();
-        await ask.delete().catch(() => {});
-
-        embed = new EmbedBuilder()
-          .setAuthor({ name: 'Registro GROTA・Meta City', iconURL: 'https://media.discordapp.net/attachments/1362573683767640215/1399820068686270484/logo_grota.png?ex=688a6387&is=68891207&hm=e4117c2559984dd42106bfff2d731c5cd3e5eb2d0f74c26c21d6db00074e3083&=&format=webp' })
-          .setColor('#F3A500')
-          .setDescription(
-            '> **Terceira Pergunta:**' +
-            '\n• *Qual o* ***ID*** *de quem te recrutou?*'
-          );
-
-        ask = await channel.send({ content: `<@${interaction.user.id}>`, embeds: [ embed ] });
-      });
-      
-
-    });
-
+    await interaction.guild.channels.cache.find((c) => c.id === config.approval_channel).send({ content: `<@${interaction.user.id}>`, embeds: [ approval ], components: [ row ] });
   } catch(err) {
     return Errors(err, `Command ${__filename}`)
       .then(() => command(client, interaction))
--- src/commands/messages/register.js
+++ src/commands/messages/register.js
@@ -1,5 +1,7 @@
-const { Errors } = require('../../utils/functions');
 const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, MessageFlags } = require('discord.js');
+
+const { Errors } = require('../../utils/functions');
+const config = require('../../../config.json');
 
 const command = async(client, interaction, args) => {
   try {
@@ -13,22 +15,23 @@ const command = async(client, interaction, args) => {
       .addComponents(register);
 
     const embed = new EmbedBuilder()
-      .setAuthor({ name: 'Grota・Meta City', iconURL: 'https://media.discordapp.net/attachments/1362573683767640215/1399820068686270484/logo_grota.png?ex=688a6387&is=68891207&hm=e4117c2559984dd42106bfff2d731c5cd3e5eb2d0f74c26c21d6db00074e3083&=&format=webp' })
-      .setThumbnail('https://media.discordapp.net/attachments/1362573683767640215/1399820068686270484/logo_grota.png?ex=688a6387&is=68891207&hm=e4117c2559984dd42106bfff2d731c5cd3e5eb2d0f74c26c21d6db00074e3083&=&format=webp')
-      .setColor('#F3A500')
+      .setAuthor({ name: config.name, iconURL: config.avatar })
+      .setThumbnail(config.avatar)
+      .setColor(config.color)
       .setDescription(
         '*Seja bem vindo há nossa facção* ***GROTA*** *da cidade* ***Meta-City***!' +
         '\n*Você precisa fazer o seu registro para poder visualizar os outros chats!*' +
         
         '\n\n> **Como se registrar?**' +
         '\n1º Clique no botão abaixo **"Fazer o Registro"**' +
-        '\n2º Será criado um chat aonde será feitos perguntas para você responder!' +
-        '\n3º Você precisará escrever o seu **ID** no chat!' +
-        '\n4º Você precisará escrever o seu **Nome** e **Sobrenome** no chat!' +
-        '\n5º Você precisará escrever o **ID** de quem te **recrutou**!' +
+        '\n2º Será criado um chat você responder algumas perguntas!' +
+        '\n3º Você precisará informar o seu **ID** da cidade!' +
+        '\n4º Você precisará informar o seu **Nome e Sobrenome** da cidade!' +
+        '\n5º Você precisará informar o seu **Número de Telefone** da cidade!' +
+        '\n6º Você precisará informar o **ID** de quem te **recrutou**!' +
 
         '\n\n> **Informações**' +
-        '\n• Após fazer o seu registro, você precisa aguardar até que um dos nossos gerentes possa aprovar o seu registro!' +
+        '\n• *Após fazer o seu registro, você precisa aguardar até que um dos nossos gerentes possa aprovar o seu registro!*' +
 
         '\n\n*__Atenciosamente Grota・Meta City__*'
       );
