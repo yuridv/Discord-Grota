5193beb4dd94cda82520f5600b40b3ea3c5d2741
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-08-22 17:50:10 -0300

    send log automatic

---
+++ src/events/messages/messageCreate.js
@@ -0,0 +1,19 @@
+const { Errors } = require('../../utils/functions');
+const config = require('../../../config.json');
+
+const { WebhookClient, EmbedBuilder } = require('discord.js');
+
+const event = (client, message) => {
+  try {
+    console.log(message.author.id, message.channel.id)
+    if (message.author.id === config.bot_webhook_chest && message.channel.id === config.logs_channel_chest) {
+      console.log('LOG CHEST')
+    }
+  } catch (err) {
+    return Errors(err, `Event ${__filename}`)
+      .then(() => event(client))
+      .catch((e) => message.channel.send(e));
+  }
+};
+
+module.exports = event;
